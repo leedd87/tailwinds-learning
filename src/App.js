6034867dcf2bc2bf01a1442997272f67b8f2fd2f
leedd87 <leedav87@gmail.com>
2022-07-30 20:39:36 -0300

    tailwinds bg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
-// import ChitChat from "./components/ChitChat";
-import NotificacionIndex from "./components/NotificacionIndex";
+import ChitChat from "./components/ChitChat";
+//import NotificacionIndex from "./components/NotificacionIndex";
 
 function App() {
 	return (
 		<div className="bg-blue-200 min-h-screen grid place-content-center">
-			{/* <ChitChat /> */}
-			<NotificacionIndex />
+			{/*<ChitChat />*/}
+			{/* <NotificacionIndex /> */}
+			<button className="bg-blue-900 px-4 py-2 rounded shadow-md focus:ring hover:bg-blue-50 transition-all active:transform active:translate-y-1">
+				Este es el mejor boton
+			</button>
 		</div>
 	);
 }
--- src/components/ChitChat.jsx
+++ src/components/ChitChat.jsx
@@ -3,15 +3,15 @@ import Bubble from "./Bubble";
 
 function ChitChat() {
 	return (
-		<div className="flex max-w-sm m-auto p-6 rounded-lg bg-white shadow-xl items-center">
+		<div className="flex max-w-sm mx-auto my-0 p-6 rounded-lg bg-white shadow-xl">
 			<div className="flex-shrink-0">
 				<picture className="w-12 h-12">
 					<Bubble color="white" />
 				</picture>
 			</div>
 			<div className="ml-6 pt-1">
-				<h4 className="text-blue-900 text-2xl">ChitChat</h4>
-				<p className="text-black-500 text-base">You have a new message!</p>
+				<h4 className="text-gray-900 text-xl">ChitChat</h4>
+				<p className="text-gray-500 text-base">You have a new message!</p>
 			</div>
 		</div>
 	);
